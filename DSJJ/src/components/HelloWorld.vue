3330dd362337437470758a6abd9b71a2fa3a5c3a
Amit Keinan <amit.keinan.9@gmail.com>
2019-03-18 20:43:46 +0200

    Create basic project and link to firebase

---
+++ DSJJ/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+ <div>
+   login<br>
+   <input v-model="email" placeholder="email"><br>
+   <input v-model="password" placeholder="password"><br>
+   <button @click="login">LOGIN!</button>
+ </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+      .then(
+        (user) => { console.log(user)},
+         (err) => { console.log(err) })
+    }
+  }
+}
+</script>
+
---
+++ DSJJ/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import firebase from 'firebase'
+import App from './App'
+import router from './router'
+import config from './firebaseConfig'
+Vue.config.productionTip = false
+
+firebase.initializeApp(config);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
