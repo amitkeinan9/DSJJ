3f36f589dd70b1250edd912c24d9142389a74be1
Amit Keinan <amit.keinan.9@gmail.com>
2019-03-25 00:30:35 +0200

    Add nice alerts and create participants page

---
+++ DSJJ/src/components/participant.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="participant">
+    <!-- <img :src="image" alt="">
+    <p>{{name}}</p>
+    <hr> -->
+    <div class="card" @click="$emit('click')">
+      <div class="card-content">
+        <article class="media">
+          <figure class="media-right">
+            <p class="image is-64x64">
+              <img :src="image">
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content has-text-right">
+              <p class="title is-4">{{name}}</p>
+              <p class="subtitle is-6">{{rank}}</p>
+              <p><strong>{{dojo}}</strong>, {{instructor}}</p>
+              
+            </div>
+          </div>
+          <!-- <div class="media-right">
+            <button class="button is-link is-outlined">ערוך</button>
+          </div> -->
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'participant',
+    props: ['name', 'image', 'dojo', 'rank', 'instructor'],
+    data() {
+      return {
+        data: {}
+      }
+    },
+    methods: {
+
+    }
+  }
+
+</script>
---
+++ DSJJ/src/pages/ParticipantPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p class="title is-1">חניך</p>
+    <br>
+    תוכן וכזה
+  </div>
+</template>
+
+<script>
+  import {
+    mapActions
+  } from 'vuex'
+
+  export default {
+    name: 'ParticipantPage',
+    components: {
+
+    },
+    data() {
+      return {
+        msg: "",
+      }
+    },
+    methods: {
+      ...mapActions(['authorizePage']),
+    },
+    created() {
+      this.authorizePage();
+      this.msg = sessionStorage.getItem('user')
+    },
+  }
+
+</script>
